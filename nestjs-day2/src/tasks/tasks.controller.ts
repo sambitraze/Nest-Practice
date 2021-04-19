1bfc4f83dd9ffcd8883c63dd0b9726e5d94dd794
sambitraze <majhisambit2@gmail.com>
2021-04-19 18:40:57 +0530

    up

--- nestjs-day2/src/tasks/tasks.controller.ts
+++ nestjs-day2/src/tasks/tasks.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[]{ 
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() body): any {
+        // console.log(body.title, body.description);
+        return this.taskService.createTask(body.title, body.description);
+    }
 }
+
